cfecb7ae94e94e1af08b5dfdfe524b24416999cd
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-05 01:55:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-  
+
   def initialize(word)
     @word = word
-  end 
+  end
 
   def match(array)
-    array.select(){|match| 
+    array.select(){|match|
       match if @word.split("").sort == match.split("").sort
     }
   end
-end 
+end
